b54a25e67d53a38a3ec4090c9768d890b204467d
Aaron White <awhite@tarvold.com>
2019-10-18 06:57:40 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,120 @@
+'use strict'
+
+// docker run -p 8000:8000 -v $(pwd)/local/dynamodb:/data/ amazon/dynamodb-local -jar DynamoDBLocal.jar -sharedDb -dbPath /data
+// sam local start-api --port 3030 --skip-pull-image
+
+const AWS = require('aws-sdk') // dev dependency to save space
+require('dotenv').config() // process.env variables
+const uuidv4 = require('uuid/v4')
+
+AWS.config.update({
+    dynamodb: { endpoint: process.env.AWS_DYNAMODB_ENDPOINT },
+    apiVersion: '2012-08-10'
+})
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+module.exports.handler = async (event) => {
+    try {
+        const persons = await docClient.scan({ TableName: "Persons" }).promise()
+        return {
+            statusCode: 200,
+            headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+            body: JSON.stringify({ persons: persons.Items })
+          }
+    } catch (err) {
+        return {
+            statusCode: 200,
+            status: 'error',
+            message: err
+        }
+    }
+}
+
+module.exports.addPerson = async (event) => {  
+    try {
+        const { body } = event
+        const input = JSON.parse(body)
+        const { firstName, lastName, age } = input
+
+        if (!firstName || !lastName || !age) {
+            return {
+                statusCode: 200,
+                headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    status: 'error',
+                    message: 'Please complete all fields'
+                })
+            }
+        }
+
+        const personKey = uuidv4()
+        const params = {
+            TableName: 'Persons',
+            Item: { personKey, firstName, lastName, age }
+        }
+        const data = await docClient.put(params).promise()
+        return {
+            statusCode: 200,
+            headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                status: 'success',
+                message: 'Member sucessfuly created',
+                personKey
+            })
+        }
+    } catch (err) {
+        return {
+            statusCode: 200,
+            headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                status: 'error',
+                message: err.message
+            })
+        }
+    }
+}
+
+module.exports.deletePerson = async (event) => {  
+    try {
+        const { body } = event
+        const input = JSON.parse(body)
+        const { personKey } = input
+
+        if (!personKey) {
+            return {
+                statusCode: 200,
+                headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    status: 'error',
+                    message: 'Please specify a person to delete'
+                })
+            }
+        }
+
+        console.log('[' + personKey)
+
+        const params = {
+            TableName: 'Persons',
+            Key: { personKey }
+        }
+        await docClient.delete(params).promise()
+        return {
+            statusCode: 200,
+            headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                status: 'success',
+                message: 'Member sucessfuly deleted',
+                personKey
+            })
+        }
+    } catch (err) {
+        return {
+            statusCode: 200,
+            headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                status: 'error',
+                message: err.message
+            })
+        }
+    }
+}
